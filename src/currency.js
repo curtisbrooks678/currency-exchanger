1b545083cfa19be3859b13d2fd55fb28252771aa
Curtis Brooks <curtisbrooks678@gmail.com>
2021-11-19 11:16:00 -0800

    add base HTML for inputs, add base jquery in index.js, link API Key to currency.js

--- src/currency.js
+++ src/currency.js
@@ -2,7 +2,7 @@ export default class Currency {
   static getCurrency() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = ``;
+      const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './currency.js'
 
-
+$(document).ready(function() {
+  let promise = Currency.getCurrency();
+  $('#exchange').click(function() {
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      const input = $('#input').val();
+      const inputCurrency = $('#inputCurrency').val();
+      const outputCurrency = $('#outputCurrency').val();
+      let inputPrice = '';
+      let outputPrice = '';
+      let output = '';
+      // for(const property in body.conversion_rates) {
+      //   if(inputCurrency === `${body[i].name}`) {
+      //     inputPrice = `${body[i].price}`;
+      //   }
+      // }
+      // for(let i=0; i<body2.length; i++) {
+      //   if(outputCurrency === `${body[i].name}`) {
+      //     outputPrice = `${body[i].price}`;
+      //   }
+      // }
+      output = (inputPrice / outputPrice) * input;
+      output = Math.round(output * 10000) / 10000;
+      $('#showExchange').show();
+      $('#showConversion').show();
+      $('#inputSpan').text(input);
+      $('#inputCurrencySpan').text(inputCurrency);
+      $('#outputSpan').text(output);
+      $('#outputCurrencySpan').text(outputCurrency);
+    });
+  });
+});
 
