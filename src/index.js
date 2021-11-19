c466ee7d143950f3cfd82ae352cd54080fb7523e
Curtis Brooks <curtisbrooks678@gmail.com>
2021-11-19 15:57:57 -0800

    add error messages if input currency and output currency are not valid currency codes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './currency.js';
 
+function clearFields() {
+  $('#currencyErrors1').text("");
+  $('#currencyErrors2').text("");
+  $('#showErrors').text("");
+}
+
 $(document).ready(function() {
-  let promise = Currency.getCurrency();
   $('#exchange').click(function() {
+    clearFields();
+    let promise = Currency.getCurrency();
     promise.then(function(response) {
       const body = JSON.parse(response);
       const input = $('#input').val();
@@ -15,26 +22,47 @@ $(document).ready(function() {
       let inputRate = '';
       let outputRate = '';
       let output = '';
+
+
+      let counter1 = 0;
       for(const code in body.conversion_rates) {
         if (`${code}` === inputCurrency) {
           inputRate = `${body.conversion_rates[code]}`;
+        } 
+        if (`${code}` !== inputCurrency) {
+          counter1++;
+        } 
+        if (counter1 === 160) {
+          $('#showConversion').hide();
+          $('#currencyErrors1').text("Your first currency code is entered incorrectly. Please enter a valid currency code from the list below.");
         }
       }
+
+      let counter2 = 0;
       for(const code2 in body.conversion_rates) {
         if (`${code2}` === outputCurrency){
           outputRate = `${body.conversion_rates[code2]}`;
+        } 
+        if (`${code2}` !== outputCurrency) {
+          counter2++;
+        } 
+        if (counter2 === 160) {
+          $('#showConversion').hide();
+          $('#currencyErrors2').text("Your second currency code is entered incorrectly. Please enter a valid currency code from the list below.");
         }
       }
       
-      output = (inputRate / outputRate) * input;
-      output = Math.round(output * 10000) / 10000;
-
-      $('#showExchange').show();
-      $('#showConversion').show();
-      $('#inputSpan').text(input);
-      $('#inputCurrencySpan').text(inputCurrency);
-      $('#outputSpan').text(output);
-      $('#outputCurrencySpan').text(outputCurrency);
+      if (counter1 < 160 && counter2 < 160) {
+        output = (inputRate / outputRate) * input;
+        output = Math.round(output * 10000) / 10000;
+        $('#showConversion').show();
+        $('#inputSpan').text(input);
+        $('#inputCurrencySpan').text(inputCurrency);
+        $('#outputSpan').text(output);
+        $('#outputCurrencySpan').text(outputCurrency);
+      }
+    }, function (error) {
+      $('#showErrors').text(`There was an error processing your request: ${error}`);
     });
   });
 });
